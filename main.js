1b653e9caebb3a5b1dab04fb2f4afdf79c7d01a8
Michael <michael@student-net-vpn-0537.ethz.ch>
2020-06-04 09:14:31 +0200

    set up

--- main.js
+++ main.js
@@ -17,10 +17,27 @@ let resulting = [10]
 
 
 
+function neuron() {
+    this.value = 0;
+    this.bias = 0;
+    
+    this.weights = [];
+}
 
 
 //sigmoid for weighted sum
 //s(x) = 1/1 + e(-x)
-function sigmoid(t) {
+function sigmoid(t)
+{
     return (1 / ( 1 + Math.pow(Math.E, -t)))
+}
+
+//calculate the activation of a neuron (value from 0 to 1)
+function act(weights, values, bias = 0)
+{
+    sum = bias;
+    for (i = 0; i < weights.length; i++){
+        sum += weights[i] * values[i]
+    }
+    return (sigmoid(sum))
 }
